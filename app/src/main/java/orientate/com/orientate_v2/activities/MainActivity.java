60540873afa8325750c07564dbc633fc1890a971
vrsa9208 <vrsa9208@gmail.com>
2018-08-26 10:47:05 -0500

    Inicio del repositorio

---
+++ app/src/main/java/orientate/com/orientate_v2/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package orientate.com.orientate_v2.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import orientate.com.orientate_v2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
